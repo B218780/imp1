15f82e41ab39f7f66447718a825a1942bac7a2c4
bhikam <mlq41204167@gmail.com>
2023-09-25 16:42:33 +0530

    input field m jese hi koi number type krenge to utne hi div bnte jaynge jitne number input m type kiya h

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [boxes, setBoxes] = useState([]);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+
+    if (!isNaN(value) && value !== '') {
+      const numberOfBoxes = parseInt(value);
+      const newBoxes = Array.from({ length: numberOfBoxes });
+      setBoxes(newBoxes);
+    } else {
+      setBoxes([]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Input Value Box Creator</h1>
+      <input
+        type="text"
+        placeholder="Enter a number"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <div className="box-container">
+        {boxes.map((_,index) => (
+          
+          <div key={index} className="box" style={{border:"2px solid red", width:"20px", height:"20px"}}>
+            {index}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
